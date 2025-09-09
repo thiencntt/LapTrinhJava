4ba7e04ef7f296729bd3c1caceaa918fde7f0e59
thiencntt <nguyenchithien@gmail.com>
2025-09-09 08:34:41 +0700

    thêm bài 2

---
+++ src/spc/edu/Bai2_NhapXuat.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spc.edu;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Bai2_NhapXuat {
+    public static void main(String[] args) {
+        System.out.println("Bai so 2");
+    }
+}
